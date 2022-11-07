027d7e4e6e401f4ec37588a9973a9451da736ac3
Dmytro Kovalenko <Dmytro.Kovalenko@microfocus.com>
2022-11-07 15:31:08 +0200

    [+/*] Book service added.

---
+++ book-service/src/main/java/com/abcloudz/springmicroservicestemplate/bookservice/exception/EntityNotFoundException.java
@@ -0,0 +1,10 @@
+package com.abcloudz.springmicroservicestemplate.bookservice.exception;
+
+import org.springframework.dao.DataAccessException;
+
+public class EntityNotFoundException extends DataAccessException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
