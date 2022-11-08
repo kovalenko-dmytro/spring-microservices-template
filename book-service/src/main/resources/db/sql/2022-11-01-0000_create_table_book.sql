55d51776e8a030af4416ffa8424b6cd0783391a4
Dmytro Kovalenko <Dmytro.Kovalenko@microfocus.com>
2022-11-08 12:56:40 +0200

    [*] Book service create table book migration fixed.

--- book-service/src/main/resources/db/sql/2022-11-01-0000_create_table_book.sql
+++ book-service/src/main/resources/db/sql/2022-11-01-0000_create_table_book.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.book (
-    book_    serial NOT NULL,
+    book_    bigserial NOT NULL,
     added_at timestamp    NOT NULL,
     author   varchar(255) NOT NULL,
     title    varchar(255) NOT NULL,
